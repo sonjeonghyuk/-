97b996424c8fdece12af1aad8e8615cd318f5788
sonjeonghyuk <132703200+sonjeonghyuk@users.noreply.github.com>
2023-06-24 18:12:35 +0900

    [level 1] Title: [1차] 비밀지도, Time: 13.24 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+            answer[i] = String.format("%"+n+"s", answer[i]);
+            answer[i] = answer[i].replace("1", "#");
+            answer[i] = answer[i].replace("0", " ");
+        }
+        
+        return answer;
+    }
+}
