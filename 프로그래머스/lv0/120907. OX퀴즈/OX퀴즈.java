f93529fbfe344e523fec67113b7f782f90c5b6cd
sonjeonghyuk <132703200+sonjeonghyuk@users.noreply.github.com>
2023-07-19 15:21:43 +0900

    [level 0] Title: OX퀴즈, Time: 0.13 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        for (int i = 0; i < quiz.length; i++) {
+            String[] str = quiz[i].split(" ");
+            String op = str[1];
+
+            switch (op){
+                case "+":
+                    answer[i] = Integer.parseInt(str[0]) + Integer.parseInt(str[2]) == Integer.parseInt(str[4]) ? "O" : "X";
+                    break;
+
+                case "-":
+                    answer[i] = Integer.parseInt(str[0]) - Integer.parseInt(str[2]) == Integer.parseInt(str[4]) ? "O" : "X";
+                    break;
+            }
+
+        }
+        return answer;
+    }
+}
