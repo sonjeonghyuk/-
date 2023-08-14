e5e6ddd47ee14129bd1eb75bdc814b2f1ed8ac23
sonjeonghyuk <132703200+sonjeonghyuk@users.noreply.github.com>
2023-08-14 17:18:06 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,6 +1,19 @@
-import java.util.stream.IntStream;
+import java.util.ArrayList;
 class Solution {
     public int[] solution(int n) {
-        return IntStream.range(1, n + 1).filter(i -> i % 2 != 0).toArray();
+     
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 != 0) {
+                list.add(i);
+            }
+        }
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
     }
 }
