900475579353216c6e1c6bd0bdc9f74b2b2ed19e
sonjeonghyuk <132703200+sonjeonghyuk@users.noreply.github.com>
2023-08-31 15:52:24 +0900

    [unrated] Title: 이어 붙인 수, Time: 1.51 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,18 +1,17 @@
 class Solution {
     public int solution(int[] num_list) {
-        StringBuilder sb = new StringBuilder();
-        StringBuilder sb1 = new StringBuilder();
+        int answer = 0;
+        String s1 = "";
+        String s2 = "";
+
         for (int i = 0; i < num_list.length; i++) {
             if (num_list[i] % 2 == 0) {
-                sb1.append(num_list[i]);
+                s2 += Integer.toString(num_list[i]);
             } else {
-                sb.append(num_list[i]);
+                s1 += Integer.toString(num_list[i]);
             }
-            System.out.println("sb = " + sb);
-            System.out.println("sb1 = " + sb1);
         }
-        int answer = Integer.parseInt(sb.toString()) + Integer.parseInt(sb1.toString());
-        
+        answer = Integer.parseInt(s1) + Integer.parseInt(s2);
         return answer;
     }
 }
