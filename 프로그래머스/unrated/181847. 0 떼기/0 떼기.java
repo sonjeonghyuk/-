c62588de91c1e063ccfe1680228a5be0728d194f
sonjeonghyuk <132703200+sonjeonghyuk@users.noreply.github.com>
2023-09-07 01:56:12 +0900

    [unrated] Title: 0 떼기, Time: 5.47 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,22 +1,6 @@
-import java.util.ArrayList;
-
 class Solution {
     public String solution(String n_str) {
-        ArrayList<String> list = new ArrayList<>();
-        String[] str = n_str.split("");
-
-        for (String item : str) {
-            list.add(item);
-        }
-
-        while (list.get(0).equals("0")) {
-            list.remove(0);
-        }
-
         String answer = "";
-        for (String item : list) {
-            answer += item;
-        }
-        return answer;
+        return ""+Integer.parseInt(n_str);
     }
 }
